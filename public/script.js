c3e3b48afd50c1707885b84fdeab233deddfbc89
pr-alves1 <76628020+pr-alves1@users.noreply.github.com>
2022-06-24 22:09:14 +0100

    Commit
    wdw

--- public/script.js
+++ public/script.js
@@ -14,7 +14,52 @@ $(document).ready(function(){
             }
 
         });
+        $.ajax({
+            url:"http://127.0.0.1/books",
+            method: "GET",
+            contentType: "application/json",
+            data: JSON.stringify({quote: value}),
+            success: function(res){
+                    $("#books").append(`
+                    <h2>Infographics books</h2>
+                    <p>Número de Books:${res.length} </p>
+                  `);
+
+            }
+
+        });
+        $.ajax({
+            url:"http://127.0.0.1/characters",
+            method: "GET",
+            contentType: "application/json",
+            data: JSON.stringify({quote: value}),
+            success: function(res){
+                    $("#characters").append(`
+                    <h2>Infographics Characters</h2>
+                    <p>Número de characters:${res.length} </p>
+                 `);
+
+            }
+
+        });
+
+        $.ajax({
+            url:"http://127.0.0.1/houses",
+            method: "GET",
+            contentType: "application/json",
+            data: JSON.stringify({quote: value}),
+            success: function(res){
+                    $("#houses").append(`
+                    <h2>Infographics Houses</h2>
+                    <p>Número de Houses:${res.length} </p>
+                  `);
+
+            }
+
+        });
+
 
     });
+    
 
 });
